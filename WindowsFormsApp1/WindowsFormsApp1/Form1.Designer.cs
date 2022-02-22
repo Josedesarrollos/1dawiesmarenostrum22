0a3bd6e057b47943e06ec79904eb1fc24e180ea9
jose <jose.b4.1992.desarrollo@gmail.com>
2022-02-22 22:40:40 +0100

    numeros cambiados

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -30,12 +30,13 @@ namespace WindowsFormsApp1
         private void InitializeComponent()
         {
             this.listaAlumnos = new System.Windows.Forms.TextBox();
-            this.aluNota = new System.Windows.Forms.TextBox();
             this.aluNombre = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.aluNota = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.aluNota)).BeginInit();
             this.SuspendLayout();
             // 
             // listaAlumnos
@@ -46,13 +47,6 @@ namespace WindowsFormsApp1
             this.listaAlumnos.ReadOnly = true;
             this.listaAlumnos.Size = new System.Drawing.Size(293, 116);
             this.listaAlumnos.TabIndex = 0;
-            // 
-            // aluNota
-            // 
-            this.aluNota.Location = new System.Drawing.Point(271, 62);
-            this.aluNota.Name = "aluNota";
-            this.aluNota.Size = new System.Drawing.Size(100, 20);
-            this.aluNota.TabIndex = 1;
             // 
             // aluNombre
             // 
@@ -98,20 +92,28 @@ namespace WindowsFormsApp1
             this.label3.TabIndex = 6;
             this.label3.Text = "Lista de alumnos";
             // 
+            // aluNota
+            // 
+            this.aluNota.Location = new System.Drawing.Point(269, 63);
+            this.aluNota.Name = "aluNota";
+            this.aluNota.Size = new System.Drawing.Size(100, 20);
+            this.aluNota.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.aluNota);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.aluNombre);
-            this.Controls.Add(this.aluNota);
             this.Controls.Add(this.listaAlumnos);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.aluNota)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,12 +122,12 @@ namespace WindowsFormsApp1
         #endregion
 
         private System.Windows.Forms.TextBox listaAlumnos;
-        private System.Windows.Forms.TextBox aluNota;
         private System.Windows.Forms.TextBox aluNombre;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown aluNota;
     }
 }
 
